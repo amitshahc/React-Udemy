07a8d4e27edfa7bac957b189bdba924c6c3e4e99
amit.shah <amit.shah@avista.com>
2018-12-13 17:35:28 +0530

    ngFor(): conditional content view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
+import person from './Person/Person';
 
 class App extends Component {
   state = {
     persons : [
       { name: "Max", age: 23 },
       { name: "Mame", age: 29 },
-    ]
+      { name: "Chrish", age: 25 },
+    ],
+    showPerson: false
   }
 
   stateChangeHandler = (newName) => {
@@ -28,13 +31,31 @@ class App extends Component {
     });
   }
 
+  togglePersons = () => {
+    let show = this.state.showPerson;
+    this.setState({showPerson: !show });
+  }
+
   render() {
+
+    const button_style = {
+      border: '1px solid blue',
+      padding: '5px'
+    };
+
+    let persons = null;
+
+    if (this.state.showPerson) {
+      persons = (<div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.stateChangeHandler.bind(this, "Maximilian")} />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} change={this.nameChangeHandler}>Meme content</Person>
+      </div> )
+    }
     return (
       <div className="App">
         <h1>My React App</h1>
-        <button onClick={this.stateChangeHandler}>Click</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.stateChangeHandler.bind(this, "Maximilian")} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} change={this.nameChangeHandler}>Meme content</Person>
+        <button onClick={this.togglePersons} style={button_style}>Click</button>
+        {persons}        
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I am a {props.name} Constant Person. Age: {props.age} </p>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name} />
