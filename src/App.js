c0cfd23d686308835b12c3041cc86cc577d42785
amit.shah <amit.shah@avista.com>
2018-12-13 17:47:18 +0530

    Outputing list: this.state.persons.map()

--- src/App.js
+++ src/App.js
@@ -47,9 +47,13 @@ class App extends Component {
 
     if (this.state.showPerson) {
       persons = (<div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.stateChangeHandler.bind(this, "Maximilian")} />
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} change={this.nameChangeHandler}>Meme content</Person>
-      </div> )
+        {
+          this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />
+          })
+        }          
+      </div>
+      )
     }
     return (
       <div className="App">
