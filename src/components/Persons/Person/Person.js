1ddcf7661f15083a8b33ec0863c92c1341ff8b5c
amit.shah <amit.shah@avista.com>
2019-01-30 18:26:20 +0530

    Convert functional component to Class based component
    -> Stateless to Stateful

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,27 +1,28 @@
 import React from 'react';
-//import { Component } from 'react';
+import { Component } from 'react';
 import './Person.css';
 
-const person = (props) => {
-        return (
-            <div className="Person">
-                <p onClick={props.click}>I am a {props.name} Constant Person. Age: {props.age} </p>
-                <p>{props.children}</p>
-                <input type="text" onChange={props.change} value={props.name} />
-            </div>        
-        );
-    }
-
-
-// class person extends Component{
-//     render() {
+// const person = (props) => {
 //         return (
-//             <div>
-//                 <p>I am a {this.props.name} Class Person. Age: {this.props.age} </p>
-//                 <p>{this.props.children}</p>
-//             </div>
+//             <div className="Person">
+//                 <p onClick={props.click}>I am a {props.name} Constant Person. Age: {props.age} </p>
+//                 <p>{props.children}</p>
+//                 <input type="text" onChange={props.change} value={props.name} />
+//             </div>        
 //         );
 //     }
-// }
 
-export default person;
+
+class Person extends Component{
+    render() {
+        return (
+            <div className="Person">
+                <p onClick={this.props.click}>I am a {this.props.name} Constant Person. Age: {this.props.age} </p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.change} value={this.props.name} />
+            </div> 
+        );
+    }
+}
+
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => 
-    props.persons.map((person, index) => {
-        return (            
-            <Person
-                key={person.id}
-                name={person.name}
-                age={person.age}                    
-                click={() => props.clicked(index)}                    
-                change={(event) => props.changed(event, person.id)}
-            />            
-        )
-    });
+// const persons = (props) => 
+//     props.persons.map((person, index) => {
+//         return (            
+//             <Person
+//                 key={person.id}
+//                 name={person.name}
+//                 age={person.age}                    
+//                 click={() => props.clicked(index)}                    
+//                 change={(event) => props.changed(event, person.id)}
+//             />            
+//         )
+//     });
 
 
-export default persons;
+class Persons extends Component{
+    render() {
+        return this.props.persons.map((person, index) => {
+            return (            
+                <Person
+                    key={person.id}
+                    name={person.name}
+                    age={person.age}                    
+                    click={() => this.props.clicked(index)}                    
+                    change={(event) => this.props.changed(event, person.id)}
+                />            
+            )
+        });       
+    }
+}
+export default Persons;
